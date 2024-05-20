c5251233067ab268eba8fc3aa30451eda69f4aac
jean-heyller <heyller-19@outlook.com>
2024-05-20 09:37:32 -0500

    the base architecture of the project is created

---
+++ microservice_small_square/microservice_small_square/src/main/java/com/example/microservice_small_square/MicroserviceSmallSquareApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservice_small_square;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceSmallSquareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceSmallSquareApplication.class, args);
+	}
+
+}
---
+++ microservice_small_square/microservice_small_square/src/test/java/com/example/microservice_small_square/MicroserviceSmallSquareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservice_small_square;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceSmallSquareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
